68443aa7439b5d38605fbf33f41f7025c1a31e28
mii443 <mii@sfc.wide.ad.jp>
2025-08-02 22:01:42 +0900

    GDT data segment

--- nel_os_kernel/src/interrupt/gdt.rs
+++ nel_os_kernel/src/interrupt/gdt.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 use x86_64::{
     instructions::tables::load_tss,
-    registers::segmentation::{Segment, CS},
+    registers::segmentation::{Segment, CS, DS, ES, FS, GS, SS},
     structures::{
         gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector},
         tss::TaskStateSegment,
@@ -30,12 +30,14 @@ lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
         let code_selector = gdt.append(Descriptor::kernel_code_segment());
+        let data_selector = gdt.append(Descriptor::kernel_data_segment());
         let tss_selector = gdt.append(Descriptor::tss_segment(&TSS));
 
         (
             gdt,
             Selectors {
                 code_selector,
+                data_selector,
                 tss_selector,
             },
         )
@@ -44,6 +46,7 @@ lazy_static! {
 
 struct Selectors {
     code_selector: SegmentSelector,
+    data_selector: SegmentSelector,
     tss_selector: SegmentSelector,
 }
 
@@ -52,6 +55,11 @@ pub fn init() {
 
     unsafe {
         CS::set_reg(GDT.1.code_selector);
+        DS::set_reg(GDT.1.data_selector);
+        ES::set_reg(GDT.1.data_selector);
+        FS::set_reg(GDT.1.data_selector);
+        GS::set_reg(GDT.1.data_selector);
+        SS::set_reg(GDT.1.data_selector);
         load_tss(GDT.1.tss_selector);
     }
 }
