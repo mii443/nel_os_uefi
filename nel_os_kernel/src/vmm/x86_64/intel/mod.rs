646d7d581d98ac584b3dd501743d8227d884a22c
Masato Imai <mii@sfc.wide.ad.jp>
2025-08-04 07:58:59 +0000

    intel

--- nel_os_kernel/src/vmm/x86_64/intel/mod.rs
+++ nel_os_kernel/src/vmm/x86_64/intel/mod.rs
@@ -1,9 +1,26 @@
-use crate::{info, vmm::VCpu};
+use raw_cpuid::cpuid;
+
+use crate::{
+    info,
+    vmm::{x86_64::common, VCpu},
+};
 
 pub struct IntelVCpu;
 
 impl IntelVCpu {
     pub fn new() -> Self {
+        let mut msr = common::read_msr(0x3a);
+        if msr & (1 << 2) == 0 {
+            msr |= 1 << 2;
+            msr |= 1;
+            common::write_msr(0x3a, msr);
+        }
+
+        let msr = common::read_msr(0x3a);
+        if msr & (1 << 2) == 0 {
+            panic!("VMX is not enabled in the BIOS");
+        }
+
         IntelVCpu
     }
 }
@@ -17,6 +34,16 @@ impl VCpu for IntelVCpu {
     where
         Self: Sized,
     {
+        if cpuid!(0x1).ecx & (1 << 5) == 0 {
+            info!("Intel CPU does not support VMX");
+            return false;
+        }
+
+        let msr = common::read_msr(0x3a);
+        if msr & (1 << 2) == 0 && msr & 1 != 0 {
+            info!("VMX is not enabled in the BIOS");
+            return false;
+        }
         true
     }
 }
